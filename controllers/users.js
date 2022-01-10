00932a386753bbaafce590242c484b9229cfdb30
Divon Bell <divonbell@Divons-MacBook-Pro.local>
2022-01-10 14:00:21 -0500

    login and sign up working

---
+++ controllers/users.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const usersRouter = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+// LOGIN ROUTES
+usersRouter.get('/login', (req, res) => {
+    res.render('login.ejs', {error: ''});
+});
+
+usersRouter.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        if(!user) return res.render('login', {error: 'Invalid Credentials'});
+        
+        const isMatched = bcrypt.compareSync(req.body.password, user.password);
+        if(!isMatched) return res.render('login', {error: 'Invalid Credentials'});
+
+        req.session.user = user._id;
+        res.redirect('/');
+    });
+});
+
+// SIGNUP ROUTES
+usersRouter.get('/signup', (req, res) => {
+    res.render('signup.ejs');
+});
+
+usersRouter.post('/signup', (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(12));
+    User.create(req.body, (err, user) => {
+        res.redirect('/login');
+    });
+});
+
+
+usersRouter.get('/logout', (req, res) => {
+    req.session.destroy(function() {
+        res.redirect('/');
+    });
+});
+
+module.exports = usersRouter;
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan')
+const methodOverride = require('method-override');
+const expressSession = require('express-session');
+
+const usersController = require('./controllers/users');
+const app = express();
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+
+const { PORT, DATABASE_URL, SECRET } = process.env;
+
+mongoose.connect(DATABASE_URL);
+
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// MIDDLEWARE
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(expressSession({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(function(req, res, next) {
+    console.log('Session Store: ', req.session);
+    next();
+});
+
+// CONTROLLERS
+// app.use('/', indexController);
+app.use('/', usersController);
+
+// Listener
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
