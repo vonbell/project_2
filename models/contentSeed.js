1304f1264eba10633e5187038e3a1ea89f316aec
vonbell <divonvbell@gmail.com>
2022-03-09 23:42:16 -0500

    edited seed content, linked materialize but needs application

--- models/contentSeed.js
+++ models/contentSeed.js
@@ -1,21 +1,21 @@
 module.exports = [
     {
       name: 'vonb',
-      avi: 'https://pbs.twimg.com/profile_images/1450158881968119813/nSrf-BSR_400x400.jpg',
-      bio: 'I am him',
-      img: 'https://images.unsplash.com/photo-1643008401979-7cdfd3f4f0f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1588&q=80',
-      caption: 'Life is the biggest party you will ever be at',
+      avi: 'https://i.imgur.com/GLdrgBw.jpg',
+      bio: 'Full Stack Developer',
+      img: 'https://images.unsplash.com/photo-1507812984078-917a274065be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2864&q=80',
+      caption: 'This is why I always get the window seat!',
     }, {
-      name: 'vonb',
-      avi: 'https://pbs.twimg.com/profile_images/1450158881968119813/nSrf-BSR_400x400.jpg',
-      bio: 'I am him',
-      img: 'https://images.unsplash.com/photo-1641977937814-160ab3062a23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-      caption: 'An apple a day will keep anyone away if you throw it hard enough',
+      name: 'hemi',
+      avi: 'https://i.imgur.com/LzwV94b.jpg',
+      bio: "I'm just hear for the treats",
+      img: 'https://images.unsplash.com/photo-1529472119196-cb724127a98e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2303&q=80',
+      caption: 'Hanging with friends.',
     }, {
-      name: 'vonb',
-      avi: 'https://pbs.twimg.com/profile_images/1450158881968119813/nSrf-BSR_400x400.jpg',
+      name: 'taylor-belle',
+      avi: 'https://i.imgur.com/7Nv9wB2.jpg',
       bio: 'I am him',
-      img: 'https://marylineg1.sg-host.com/blog/wp-content/uploads/2017/08/Los-angeles-@florianmerckx-2.jpg',
-      caption: 'Give second chances but not for the same mistake',
+      img: 'https://images.unsplash.com/photo-1628336707631-68131ca720c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+      caption: "Here's to another year around the sun...",
     }
 ];
